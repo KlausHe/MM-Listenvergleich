c4bda5da3c55042be4e41f3fc6a57ecc30f5e438
klausHe <klausheuken@gmx.de>
2024-02-10 22:31:33 +0100

    new design, fully functioning

--- Data/KadUtils.js
+++ Data/KadUtils.js
@@ -20,25 +20,10 @@ export function daEL(id, type, fn) {
 export function hostDebug() {
 	return ["local", "127.0.0.1"].some((s) => window.location.hostname.includes(s));
 }
-export async function socketPost(app = null, data, callback) {
-	if (app == null) return;
-	if (typeof data != "object") {
-		console.error("NO OBJECT", data);
-		return;
-	}
-	const path = "K-Universe/";
-	const request = new Request(`${path}${app}/`, {
-		method: "POST",
-		body: data == null ? "" : JSON.stringify(data),
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-	});
-	const response = await fetch(request);
-	const obj = await response.json();
-	callback(obj);
+export function objectLength(obj) {
+	return Object.keys(obj).length;
 }
+
 export function deepClone(data) {
 	if (data === null || data === undefined) return data;
 	return JSON.parse(JSON.stringify(data));
--- Main.js
+++ Main.js
@@ -1,22 +1,27 @@
 import * as KadUtils from "./Data/KadUtils.js";
-import { utils, read, writeFile } from "https://cdn.sheetjs.com/xlsx-0.20.1/package/xlsx.mjs";
+import { utils, read, writeFile } from "./Data/xlsx.mjs";
 
 window.onload = mainSetup;
 
 function mainSetup() {
-	KadUtils.KadDOM.resetInput(idVin_customerNumber, "Beistellnummer", { min: 0, max: 999999 });
-	KadUtils.daEL(idVin_customerNumber, "input", getCustomerNumber);
-	dashZeroState = KadUtils.KadDOM.resetInput(idCB_dashZero, false);
-	KadUtils.daEL(idCB_dashZero, "change", dashZeroFilter);
-
 	KadUtils.dbID(idLbl_loadedSOU).textContent = "nicht geladen";
 	KadUtils.dbID(idLbl_loadedCAD).textContent = "nicht geladen";
-	KadUtils.dbID(idLbl_compare).textContent = "Daten hochladen";
-
 	KadUtils.daEL(idVin_inputSOU, "change", (evt) => getFile(evt, "SOU"));
 	KadUtils.daEL(idVin_inputCAD, "change", (evt) => getFile(evt, "TEXT"));
-	KadUtils.daEL(idBtn_compare, "click", startCompare);
-	KadUtils.KadDOM.enableBtn(idBtn_compare, false);
+
+	KadUtils.KadDOM.resetInput(idArea_customNumbers, "MM-Nummern in CAD-Datei ignorieren");
+	KadUtils.dbID(idLbl_customNumbers).textContent = "...";
+	KadUtils.daEL(idArea_customNumbers, "input", getcustomNumbers);
+	stateIgnoreAssembly = KadUtils.KadDOM.resetInput(idCB_ignoreAssembly, true);
+	KadUtils.daEL(idCB_ignoreAssembly, "change", ignoreAssemblyFilter);
+	stateDashZero = KadUtils.KadDOM.resetInput(idCB_dashZero, false);
+	KadUtils.daEL(idCB_dashZero, "change", dashZeroFilter);
+
+	KadUtils.daEL(idBtn_refreshParsing, "click", refreshParsing);
+
+	KadUtils.dbID(idLbl_errorsFound).textContent = "...";
+	KadUtils.daEL(idBtn_download, "click", startDownload);
+	KadUtils.KadDOM.enableBtn(idBtn_download, false);
 }
 
 const mmID = "ArtikelNr";
@@ -26,25 +31,48 @@ const partFamily = "ArtikelTeileFamilie";
 const filterFamily = ["Rohmaterial", "Baugruppe"];
 const parsedHeader = [mmID, "SOU", "CAD", name, partFamily];
 
-let customerNumber = "";
-let dashZeroState = false;
+let customNumbers = null;
+let stateDashZero = false;
+let stateIgnoreAssembly = false;
 let filedataSOU = null;
 let filedataCAD = null;
 let objectListSOU = {};
 let objectListCAD = {};
 let objectListCompared = {};
 let objectListDifferences = {};
+let dataDiff = [];
+let dataComp = [];
 let filesParsed = {
 	SOU: false,
 	CAD: false,
 };
 
-function getCustomerNumber(event) {
-	customerNumber = event.target.valueAsNumber;
+function getcustomNumbers(event) {
+	let results = event.target.value;
+	customNumbers = results.match(/\d{6}/g);
+	let text = "";
+	if (customNumbers == null) {
+		KadUtils.dbID(idLbl_customNumbers).textContent = "...";
+		return;
+	}
+	customNumbers = customNumbers.map((n) => Number(n));
+	text = `${customNumbers.length} Nummern ignoriert:`;
+	for (let t of customNumbers) {
+		text += `\n ${t}`;
+	}
+	KadUtils.dbID(idLbl_customNumbers).textContent = text;
 }
 
 function dashZeroFilter(event) {
-	dashZeroState = event.target.checked;
+	stateDashZero = event.target.checked;
+}
+function ignoreAssemblyFilter(event) {
+	stateIgnoreAssembly = event.target.checked;
+}
+
+function refreshParsing() {
+	if (filesParsed.SOU) parseFileExcel();
+	if (filesParsed.CAD) parseFileText();
 }
 
 function getFile(evt, type) {
@@ -66,14 +94,6 @@ function getFile(evt, type) {
 	}
 }
 
-function fileIsLoaded(type) {
-	filesParsed[type] = true;
-
-	const count = type == "SOU" ? objLength(objectListSOU) : objLength(objectListCAD);
-	KadUtils.dbID(`idLbl_loaded${type}`).textContent = `${count} Teile geladen`;
-	if (filesParsed.SOU && filesParsed.CAD) KadUtils.KadDOM.enableBtn(idBtn_compare, true);
-}
-
 function parseFileExcel() {
 	let workbook = read(filedataSOU, { type: "binary" });
 	workbook.SheetNames.forEach((sheet) => {
@@ -90,7 +110,7 @@ function parseFileExcel() {
 				}
 			}
 		}
-		fileIsLoaded("SOU");
+		fileIsParsed("SOU");
 	});
 }
 
@@ -98,22 +118,24 @@ function parseFileText() {
 	objectListCAD = {};
 	for (let row of filedataCAD) {
 		if (row.trim() == "") continue;
-		// const depth = row.search(/\S|$/) / 3;
 		let text = row.trim();
 		if (text.trim().substring(0, 3) === "|--") text = text.trimStart().slice(3); // remove "|--"
 
-		if (cleanParentheses(text)) continue; // igrnore drawings
+		if (cleanParentheses(text)) continue; // ignore drawings and exemplares
+		if (text.search(/\D/) != 6) continue; // ignore rows with non starting 6 digit number
 
-		const id = Number(text.slice(0, 6)); // get MM-Nummer
-		if (Number.isNaN(id)) continue; // ignore MM-Nummer is NaN
-
-		if (id == customerNumber) continue;
-		if (id % 10 == 0 && filterFamily.includes("Baugruppe")) continue; //  ignore filter "Baugruppe"
+		let id = Number(text.slice(0, 6)); // get MM-Nummer
+		if (Number.isNaN(id)) {
+			continue; // ignore MM-Nummer is NaN
+		} else {
+			id = id.toString().padStart(6, "0");
+		}
+		if (cleanBaugruppe(id)) continue; // ignore Baugruppe
+		if (cleanCustomNumbers(id)) continue; // ignore customNumbers
 
 		text = text.replace(/^\d{6}/, ""); // remove MM-Nummer
 
-		if (dashZeroState && text.trim().substring(0, 2) === "-0") continue; //  ignore filter "-0"
-
+		if (cleanDashZero(text)) continue; // ignore DashZero
 		text = text.split("[")[0].trim(); // remove trailing [xx]
 
 		const obj = {
@@ -127,21 +149,43 @@ function parseFileText() {
 			objectListCAD[id][count]++;
 		}
 	}
-	fileIsLoaded("CAD");
+	fileIsParsed("CAD");
 }
 
+function fileIsParsed(type) {
+	filesParsed[type] = true;
+	const count = type == "SOU" ? KadUtils.objectLength(objectListSOU) : KadUtils.objectLength(objectListCAD);
+	KadUtils.dbID(`idLbl_loaded${type}`).textContent = `${count} Teile geladen`;
+	KadUtils.dbIDStyle(`idLbl_input${type}`).backgroundColor = "limegreen";
+
+	if (filesParsed.SOU && filesParsed.CAD) {
+		startCompare();
+	}
+}
+
+// CLEANING -----------------------------
 function cleanParentheses(text) {
 	if (text.match(/\[\]/)) return true; //cleanDrawings
 	if (text.match(/\(\d{0,}\)/)) return true; //cleanDrawings
 	return false;
 }
+function cleanBaugruppe(id) {
+	if (!stateIgnoreAssembly) return false;
+	if (id % 10 != 0) return false;
+	return filterFamily.includes("Baugruppe");
+}
+function cleanCustomNumbers(id) {
+	if (customNumbers == null) return false;
+	let s = customNumbers.includes(Number(id));
+	return s;
+}
+function cleanDashZero(text) {
+	if (!stateDashZero) return false;
+	return text.trim().substring(0, 2) === "-0";
+}
 
+// -----------------------------
 function startCompare() {
-	if (objLength(objectListCompared) > 0) {
-		parseFileExcel();
-		parseFileText();
-	}
-
 	objectListCompared = {};
 	for (let [souKey, souValue] of Object.entries(objectListSOU)) {
 		const cadCount = objectListCAD[souKey] == null ? 0 : objectListCAD[souKey][count];
@@ -150,31 +194,27 @@ function startCompare() {
 			SOU: souValue[count],
 			CAD: cadCount,
 			[name]: souValue[name],
-			[partFamily]: souValue[partFamily],
+			[partFamily]: souValue[partFamily] ? souValue[partFamily] : "---",
 		};
 	}
 
 	for (let [cadKey, cadValue] of Object.entries(objectListCAD)) {
 		if (objectListCompared[cadKey] === undefined) {
 			const souCount = objectListSOU[cadKey] == null ? 0 : objectListSOU[cadKey][count];
-			const souPart = objectListSOU[cadKey] == null ? "" : objectListSOU[cadKey][partFamily];
 			objectListCompared[cadKey] = {
 				[mmID]: cadValue[mmID],
 				SOU: souCount,
 				CAD: cadValue[count],
 				[name]: cadValue[name],
-				[partFamily]: souPart,
+				[partFamily]: "aus CAD",
 			};
 		}
 	}
 
 	objectListDifferences = Object.values(objectListCompared).filter((obj) => obj.SOU != obj.CAD);
-	createFile();
-}
 
-function createFile() {
-	let dataDiff = [];
-	let dataComp = [];
+	dataDiff = [];
+	dataComp = [];
 
 	for (let [index, listItem] of Object.values(objectListDifferences).entries()) {
 		dataDiff[index] = [];
@@ -188,23 +228,26 @@ function createFile() {
 		for (let h of parsedHeader) {
 			dataComp[index].push(listItem[h]);
 		}
-  }
-
-	KadUtils.dbID(idLbl_compare).textContent = `${dataDiff.length} Fehler gefunden!`;
-
+	}
 	dataDiff.sort((a, b) => a[0] - b[0]);
 	dataComp.sort((a, b) => a[0] - b[0]);
 
+	KadUtils.dbID(idLbl_errorsFound).textContent = `${dataDiff.length} Fehler gefunden!`;
+
+	KadUtils.KadDOM.enableBtn(idBtn_download, true);
+	console.log(dataDiff, dataComp);
+}
+
+function startDownload() {
+	refreshParsing();
+
 	dataDiff.unshift(parsedHeader);
 	dataComp.unshift(parsedHeader);
+	console.log(dataDiff, dataComp);
 	const book = utils.book_new();
 	let sheetDiff = utils.aoa_to_sheet(dataDiff);
 	let sheetComp = utils.aoa_to_sheet(dataComp);
 	utils.book_append_sheet(book, sheetDiff, "Differenz");
 	utils.book_append_sheet(book, sheetComp, "Alles");
 	writeFile(book, "Stücklistenvergleich.xlsx");
-}
-
-function objLength(obj) {
-	return Object.keys(obj).length;
 }
